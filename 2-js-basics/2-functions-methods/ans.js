983ff7e99c92cfa05514e832ced3c8d0719af780
Alvalen <109880628+Alvalens@users.noreply.github.com>
2023-01-29 11:45:09 +0700

    Create ans.js

---
+++ 2-js-basics/2-functions-methods/ans.js
@@ -0,0 +1,11 @@
+function test(param, params) {
+  console.log(`${param}, ${params}`);
+}
+
+test("hello", "you")
+
+function haha (param){
+    return `${param}, fool`
+}
+
+console.log(haha('you'))
